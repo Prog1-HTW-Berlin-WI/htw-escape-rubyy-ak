f760f73c7aa7fdceecb3ca969aeacfdb336dade3
onur62ky <Onur.Koc@student.htw-berlin.de>
2026-01-14 17:28:38 +0100

    Vollständig erledigte Klasse Hero

--- model/Hero.java
+++ model/Hero.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Random;
 
 /**
  * Repräsentiert die Spielfigur (Hero) im Spiel
@@ -14,11 +15,29 @@ public class Hero implements Serializable {
     // konsistent bleibt und Versionierungsprobleme vermieden werden.
     private static final long serialVersionUID = 3578735620108186013L;
 
+    // Maximale Lebenspunkte des Helden
+    private static final int Max_HP = 50;
+    private static final int Signatures = 5; //
+
     // Name des Spielers
     private String name;
 
+    // Attribute des Helden
+    private int healthPoints = Max_HP; // Aktuelle Lebenspunkte
+    private int experiencePoints = 0; // Aktuelle Erfahrungspunkte
+    private Lecturer[] signedExerciseLeaders; // Unterschriebene Übungsleiter
+
+    private boolean shortRestUsed; // Ob kurze Verschnaufspause genutzt wurde
+    
+    private final Random random = new Random(); // Zufallsgenerator
+
     // Konstruktor
     public Hero() {
+        this.name = "";
+        this.healthPoints = Max_HP;
+        this.experiencePoints = 0;
+        this.signedExerciseLeaders = new Lecturer[Signatures];
+        this.shortRestUsed = false;
     }
 
     // Getter für den Namen
@@ -30,4 +49,34 @@ public class Hero implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    public void takeDamage(int amount) {
+        this.healthPoints -= amount;
+        if (this.healthPoints < 0) { // Verhindert negative Lebenspunkte
+            this.healthPoints = 0;
+        }
+    }
+
+    public void regenerate(boolean longRest) {
+        int amount;
+        if (longRest) { 
+            amount = 10; // verbraucht eine ganze Runde
+        } else { // wenn Nutzer kurze Verschnaufpause wählt
+            if (shortRestUsed) {
+                return; // kurze Verschnaufpause bereits genutzt -> abbruch
+        }
+        shortRestUsed = true; // kurze Verschnaufpause wird genutzt
+        amount = 3;
+        }
+
+        healthPoints += amount;
+        if (healthPoints > Max_HP) { // Verhindert Überschreitung der maximalen Lebenspunkte
+            healthPoints = Max_HP;
+        }
+    }
+
+    public void StartNewRound() {
+        shortRestUsed = false; // kurze Verschnaufpause zurücksetzen
+    }
+
 }
